0f0fba099039fa0102b8f9769becba56e9e2c8c3
Leonid Skrebnev <ristalvtgames@yandex.com>
2023-03-20 10:13:47 +0300

    утилитарные функции сделаны

---
+++ src/utilities/__init__.py
@@ -0,0 +1,3 @@
+from .clear import clear
+from .get_script_dir import get_script_dir
+from .config import Config
---
+++ src/utilities/get_script_dir.py
@@ -0,0 +1,13 @@
+import inspect
+import os
+import sys
+
+
+def get_script_dir(follow_symlinks=False):
+    if getattr(sys, 'frozen', False):  # py2exe, PyInstaller, cx_Freeze
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
